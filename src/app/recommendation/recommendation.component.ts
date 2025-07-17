f93b8e330efbd222fac5123b71d9c5a30eb5a3b6
Jana Schueler <janaschueler@protonmail.com>
2025-07-17 10:03:26 +0200

    update mobile optimisation

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recommendation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recommendation.component.html',
-  styleUrl: './recommendation.component.scss'
+  styleUrl: './recommendation.component.scss',
 })
 export class RecommendationComponent {
-
+  testimonials = [
+    {
+      name: 'Max Name',
+      project: 'company/project',
+      quote: 'short quote highlighting my soft skills ',
+    },
+    {
+      name: 'Max Name',
+      project: 'company/project',
+      quote: 'short quote highlighting my soft skills ',
+    },
+    {
+      name: 'Max Name',
+      project: 'company/project',
+      quote: 'short quote highlighting my soft skills ',
+    },
+  ];
 }
